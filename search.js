9046bfc8674f36489b32c1adc5b977b868fcacde
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-09-14 20:35:16 -0400

    Added variables and button functionality
    Added variables in the js file that read from the html file. Buttons now run the js functions when clicked.

--- search.js
+++ search.js
@@ -1,7 +1,13 @@
+var nameInput = document.getElementById("s-name");
+var numInput = document.getElementById("s-num");
+var filter = input.value.toUpperCase();
+var ul = document.getElementById("pokedex");
+var li = ul.getElementsByTagName("li");
+
 function nameSearch(){
-    
+    console.log("Test name");        
 }
 
 function numSearch(){
-
+    console.log("Test num");
 }
