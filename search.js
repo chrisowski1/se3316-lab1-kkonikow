db0a18e2c6e173f765194678adf253e4b15d874b
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-09-15 20:35:59 -0400

    Number search
    Added number search functionality including popup and validation.

--- search.js
+++ search.js
@@ -18,22 +18,30 @@ function nameSearch() {
       num++;
       if (num == 5) {
         vali = false;
-      } else {
-
       }
-    }
-  }  
-  if(pokemon.length == 1){
+    }else if(nameInput.length >= 20) {
+        alert("Cannot enter more than 20 characters");
+        break;
+    }else if(!nameInput.match(checkLetters)) {
+        alert("Must enter only a-z and A-Z");
+        break;
+    }else if(nameInput.length >= 20 && !nameInput.match(checkLetters)) {
+        alert("Cannot enter more than 20 characters \n Must enter only a-z and A-Z");
+        break;
+    }    
+  }
+
+  if(pokemon.length == 1) {
     alert(pokemon.length + " matches:\n" + pokemon[0]);
-  }else if(pokemon.length == 2){
+  }else if(pokemon.length == 2) {
     alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1]);
-  }else if(pokemon.length == 3){
+  }else if(pokemon.length == 3) {
     alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2]);
-  }else if(pokemon.length == 4){
+  }else if(pokemon.length == 4) {
     alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3]);
-  }else if(pokemon.length == 5){
+  }else if(pokemon.length == 5) {
     alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3] + "\n" + pokemon[4]);
-  }else{
+  }else {
     alert("No matches found");
   }
 }
@@ -44,10 +52,40 @@ function numSearch() {
   numInput = numInput.value;
   var ul = document.getElementById("pokedex");
   var li = ul.getElementsByTagName("li");
+  var span;
+  var numSearch;
+  var num = 0;
+  var vali = true;
+  var pokemon = [];
 
-  if (numInput >= 1 && numInput <= 20) {
-    console.log("Valid");
-  } else {
-    console.log("Invalid");
+  for (i = 0; i < li.length; i++) {
+    span = li[i].getElementsByTagName("span")[0];
+    numSearch = span.textContent || span.innerText;
+    if (numInput >= 1 && numInput <= 20 && numSearch.indexOf(numInput) > -1 && vali) {
+      pokemon.push(numSearch);
+      num++;
+      if (num == 5) {
+        vali = false;
+      }
+    }else if(numInput < 1 || numInput > 20) {
+        alert("Must enter number 1-20");
+        break;
+    }
   }
+
+  if(pokemon.length == 1) {
+    alert(pokemon.length + " matches:\n" + pokemon[0]);
+  }else if(pokemon.length == 2) {
+    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1]);
+  }else if(pokemon.length == 3) {
+    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2]);
+  }else if(pokemon.length == 4) {
+    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3]);
+  }else if(pokemon.length == 5) {
+    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3] + "\n" + pokemon[4]);
+  }else {
+    alert("No matches found");
+  }     
 }
+
+//to do: search when enter key is pressed, add pokemon descriptions to search
