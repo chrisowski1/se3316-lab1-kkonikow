5d107b8a91d9c8dc79a4f97988d6d524c9236e94
chrisowski1 <35178770+chrisowski1@users.noreply.github.com>
2020-09-17 16:32:37 -0400

    Added desriptions to popup
    Added pokemon descriptions to popup by adding new array into js file.

--- search.js
+++ search.js
@@ -1,3 +1,24 @@
+var pokemon = ["#01 Bulbasaur   Type: Grass/Poison   Parent: N/A", 
+  "#02 Ivysaur   Type: Grass/Poison   Parent: Bulbasaur", 
+  "#03 Venusaur   Type: Grass/Poison   Parent: Ivysaur", 
+  "#04 Charmander   Type: Fire   Parent: N/A", 
+  "#05 Charmeleon   Type: Fire   Parent: Charmander", 
+  "#06 Charizard   Type: Fire/Flying   Parent: Charmeleon", 
+  "#07 Squirtle   Type: Water   Parent: N/A", 
+  "#08 Wartortle   Type: Water   Parent: Squirtle", 
+  "#09 Blastoise   Type: Water   Parent: Wartortle", 
+  "#10 Caterpie   Type: Bug   Parent: N/A", 
+  "#11 Metapod   Type: Bug   Parent: Caterpie", 
+  "#12 Butterfree   Type: Bug/Flying   Parent: Metapod", 
+  "#13 Weedle   Type: Bug/Poison   Parent: N/A", 
+  "#14 Kakuna   Type: Bug/Poison   Parent: Weedle", 
+  "#15 Beedrill   Type: Bug/Poison   Parent: Kakuna", 
+  "#16 Pidgey   Type: Normal/Flying   Parent: N/A", 
+  "#17 Pidgeotto   Type: Normal/Flying   Parent: Pidgey", 
+  "#18 Pidgeot   Type: Normal/Flying   Parent: Pidgeotto", 
+  "#19 Rattata   Type: Normal   Parent: N/A", 
+  "#20 Raticate   Type:  Normal   Parent: Rattata", ];
+
 function nameSearch() {
   var nameInput = document.getElementById("s-name");
   nameInput = nameInput.value.toUpperCase();
@@ -7,40 +28,40 @@ function nameSearch() {
   var span;
   var textSearch;
   var num = 0;
-  var vali = true;
-  var pokemon = [];
+  var vali = true;  
+  var pokemonSearch = [];
 
   for (i = 0; i < li.length; i++) {
     span = li[i].getElementsByTagName("span")[0];
     textSearch = span.textContent || span.innerText;
     if (nameInput.match(checkLetters) && nameInput.length < 20 && textSearch.toUpperCase().indexOf(nameInput) > -1 && vali) {
-      pokemon.push(textSearch);
+      pokemonSearch.push(i);
       num++;
       if (num == 5) {
         vali = false;
       }
+    }else if(nameInput.length >= 20 && !nameInput.match(checkLetters)) {
+        alert("Cannot enter more than 20 characters \nMust enter only a-z and A-Z");
+        break;
     }else if(nameInput.length >= 20) {
         alert("Cannot enter more than 20 characters");
         break;
     }else if(!nameInput.match(checkLetters)) {
         alert("Must enter only a-z and A-Z");
         break;
-    }else if(nameInput.length >= 20 && !nameInput.match(checkLetters)) {
-        alert("Cannot enter more than 20 characters \n Must enter only a-z and A-Z");
-        break;
-    }    
+    }   
   }
 
-  if(pokemon.length == 1) {
-    alert(pokemon.length + " matches:\n" + pokemon[0]);
-  }else if(pokemon.length == 2) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1]);
-  }else if(pokemon.length == 3) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2]);
-  }else if(pokemon.length == 4) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3]);
-  }else if(pokemon.length == 5) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3] + "\n" + pokemon[4]);
+  if(pokemonSearch.length == 1) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]]);    
+  }else if(pokemonSearch.length == 2) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]]);
+  }else if(pokemonSearch.length == 3) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]]);    
+  }else if(pokemonSearch.length == 4) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]] + "\n" + pokemon[pokemonSearch[3]]);
+  }else if(pokemonSearch.length == 5) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]] + "\n" + pokemon[pokemonSearch[3]] + "\n" + pokemon[pokemonSearch[4]]);
   }else {
     alert("No matches found");
   }
@@ -55,14 +76,14 @@ function numSearch() {
   var span;
   var numSearch;
   var num = 0;
-  var vali = true;
-  var pokemon = [];
+  var vali = true;  
+  var pokemonSearch = [];
 
   for (i = 0; i < li.length; i++) {
     span = li[i].getElementsByTagName("span")[0];
     numSearch = span.textContent || span.innerText;
     if (numInput >= 1 && numInput <= 20 && numSearch.indexOf(numInput) > -1 && vali) {
-      pokemon.push(numSearch);
+      pokemonSearch.push(i);
       num++;
       if (num == 5) {
         vali = false;
@@ -73,19 +94,19 @@ function numSearch() {
     }
   }
 
-  if(pokemon.length == 1) {
-    alert(pokemon.length + " matches:\n" + pokemon[0]);
-  }else if(pokemon.length == 2) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1]);
-  }else if(pokemon.length == 3) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2]);
-  }else if(pokemon.length == 4) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3]);
-  }else if(pokemon.length == 5) {
-    alert(pokemon.length + " matches:\n" + pokemon[0] + "\n" + pokemon[1] + "\n" + pokemon[2] + "\n" + pokemon[3] + "\n" + pokemon[4]);
+  if(pokemonSearch.length == 1) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]]);    
+  }else if(pokemonSearch.length == 2) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]]);
+  }else if(pokemonSearch.length == 3) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]]);    
+  }else if(pokemonSearch.length == 4) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]] + "\n" + pokemon[pokemonSearch[3]]);
+  }else if(pokemonSearch.length == 5) {
+    alert(pokemonSearch.length + " matches:\n" + pokemon[pokemonSearch[0]] + "\n" + pokemon[pokemonSearch[1]] + "\n" + pokemon[pokemonSearch[2]] + "\n" + pokemon[pokemonSearch[3]] + "\n" + pokemon[pokemonSearch[4]]);
   }else {
     alert("No matches found");
-  }     
+  }    
 }
 
-//to do: search when enter key is pressed, add pokemon descriptions to search
+
